7a335dd92c64ca6f098bd8e25cd1de0091f95585
Skyler Kroll <skylerkroll@Skylers-MacBook-Pro.local>
2025-09-04 17:34:44 -0700

    Site Complete

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+      var email = document.getElementById("email").value;
+      var confirmEmail = document.getElementById("confirmEmail").value;
+
+      if (email !== confirmEmail) {
+        alert("Email addresses do not match. Please re-enter.");
+        event.preventDefault();
+      }
+    });
